a82d04de45b9a0f0214a39a1a20e0a04da4678d1
Ortal Wikoff <owikoff@gmail.com>
2022-05-14 18:16:18 -0400

    password generator with all the files

---
+++ Develop/script.js
@@ -0,0 +1,87 @@
+// Assignment code here
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialChara = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
+
+var pwLength; 
+var pushLower;
+var pushUpper;
+var pushNumber;
+var pushSpecialChara;
+var pwChoice = []; 
+var selection = [];
+
+ 
+var generateBtn = document.querySelector("#generate");
+// Get references to the #generate element
+
+generateBtn.addEventListener("click", writePassword);
+
+function writePassword() {
+  var password = generatePw();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+}
+
+function generatePw(){
+    pwLength = prompt("Choose characters between 8-128")
+    if(pwLength < 8 || pwLength > 128){
+    pwLength = prompt ("Required corect value!")
+    return null} 
+    pushLower = confirm("Do you want your PW to contain lower case letters?");
+    pushUpper = confirm("Do you want your PW to contain upper case letters?");
+    pushNumber = confirm("Do you want your PW to contain numbers?");
+    pushSpecialChara = confirm("Do you want your PW to contain special charaters?"); 
+  
+// for all of the above true
+  if (pushNumber && pushSpecialChara && pushUpper && pushLower) {
+  selection = specialChara.concat(number, lowerCase, upperCase);
+}
+// three selected
+  else if (pushNumber && pushSpecialChara && pushUpper) {
+  selection = specialChara.concat(number, upperCase);
+} else if (pushSpecialChara && pushUpper && pushLower) {
+  selection = specialChara.concat(lowerCase, upperCase);
+} else if (pushUpper && pushLower && pushNumber) {
+  selection = upperCase.concat(lowerCase, number);
+} else if (pushLower && pushNumber && pushSpecialChara) {
+  selection = letters.concat(number, specialChara);
+}
+//two selected
+  else if (pushSpecialChara && pushNumber) {
+  selection = specialChara.concat(number);
+} else if (pushLower && pushUpper) {
+  selection = number.concat(upperCase);
+} else if (pushUpper && pushLower) {
+  selection = upperCase.concat(lowerCase);
+} else if (pushLower && number) {
+  selection = lowerCase.concat(number);
+} else if (pushLower && pushSpecialChara) {
+  selection = lowerCase.concat(specialChara);
+} else if (pushSpecialChara && pushUpper) {
+  selection = specialChara.concat(upperCase);
+}
+//one option
+  else if (pushSpecialChara) {
+  selection = specialChara;
+} else if (pushNumber) {
+  selection = number;
+} else if (pushLower) {
+  selection = lowerCase;
+} else if (pushUpper) {
+  selection = upperCase;
+};
+
+// Add event listener to generate button
+  var pwBlank = "";
+  for (var i = 0; i < pwLength; i++){
+  var randomIndex = selection[Math.floor(Math.random()* selection.length)];
+  console.log(selection); 
+  console.log(randomIndex);
+  pwChoice.push(randomIndex); 
+  console.log(pwChoice); 
+}
+return pwChoice.join("");
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+# Password Generator Starter Code
+I created application that an employee can use to generate a random password based on criteria they've selected. This app will run in the browser, and will feature dynamically updated HTML and CSS powered by JavaScript code that I wrote. 
+
+Here is a link to the webpage:
+
+
+Here is a screenshot of the first page: 
+/Users/ortalwikoff/BootCamp/OrtalHomework/3-friendly-parakeet/Develop/firstPage.png
+
